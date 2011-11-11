2e3e39a5731ba60eaed6ec25e66dc1dd74bbe1a2
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-11-11 11:55:55 -0500

    cleanup

--- pktree.hs
+++ pktree.hs
@@ -6,15 +6,6 @@ type Point = [Float] -- http://hackage.haskell.org/package/tagged-list ?
 type Rectangle = (Point, Point)
 type PKTree = Tree Rectangle
 
-k :: Int
-k = 2
-
-r :: [Int]
-r = [2,2]
-
-root :: PKTree
-root = cell ([-180, -90], [180, 90])
-
 nreplace :: (a -> a) -> Int -> [a] -> [a]
 nreplace _ _ [] = error "Tried to replace a member not in the list"
 nreplace f i (l:ls)
@@ -23,6 +14,51 @@ nreplace f i (l:ls)
 
 npartition :: (a -> Int) -> [a] -> [[a]]
 npartition f = foldr (\x -> nreplace (x:) (f x)) (repeat [])
+
+rectContains :: Rectangle -> Rectangle -> Bool
+rectContains (l, u) (l', u') =
+	and (compareEach (<) l l') && and (compareEach (>) u u')
+	where
+	compareEach _ [] _ = []
+	compareEach _ _ [] = []
+	compareEach f (a:as) (b:bs) = f a b : compareEach f as bs
+
+cell :: Rectangle -> PKTree
+cell rect = Node {
+	rootLabel = rect,
+	subForest = []
+}
+
+pointCell :: Point -> PKTree
+pointCell p = cell (p, p)
+
+insert :: Int -> [Int] -> PKTree -> Point -> PKTree
+insert k r (Node {rootLabel = (l, u), subForest = children}) p =
+	Node {
+		rootLabel = (l, u),
+		subForest = instantiateDivisions k r (l, u) newKids
+	}
+	where
+	newKids = insert' k r children p
+
+-- Takes the list of children from some node and inserts a Point
+-- either as a child, or into a child
+insert' :: Int -> [Int] -> [PKTree] -> Point -> [PKTree]
+insert' k r children p
+	| children `contains` p =
+		let (Just c) = children `maybecontain` p in
+			let c' = insert k r c p in
+				-- If c' has less than k children, it is not k-instantiable
+				(if length (subForest c') < k then
+					subForest c'
+				else
+					[c']
+				) ++ filter (/= c) children
+	| otherwise =
+		pointCell p : children
+	where
+	contains f p = isJust (maybecontain f p)
+	maybecontain f p = find (\x -> rectContains (rootLabel x) (p,p)) f
 
 divideUp :: [Int] -> [Float] -> Point -> [PKTree] -> [[PKTree]]
 divideUp r w l children =
@@ -38,16 +74,16 @@ divideUp r w l children =
 		(factor * floor ((x-l)/w), factor*(r+1))
 	dimData = zip3 r w l
 
-instantiateSubdivisions :: Rectangle -> [PKTree] -> [PKTree]
-instantiateSubdivisions (l, u) children =
+instantiateDivisions :: Int -> [Int] -> Rectangle -> [PKTree] -> [PKTree]
+instantiateDivisions k r (l, u) children =
 	concat rest ++ concatMap (\div ->
-		let newKids = instantiateSubdivisions (divBox div) div in
+		let newKids = instantiateDivisions k r (divBox div) div in
 			if length newKids < k then
 				newKids
 			else
 				[Node {
 					rootLabel = divBox div,
-					subForest = instantiateSubdivisions (divBox div) div
+					subForest = newKids
 				}]
 	) subdivisions
 	where
@@ -60,48 +96,3 @@ instantiateSubdivisions (l, u) children =
 	(subdivisions, rest) =
 		partition (\x -> length x >= k) (divideUp r w l children)
 	w = map (\(l,u,r) -> (u-l) / fromIntegral r) (zip3 l u r)
-
-insert :: PKTree -> Point -> PKTree
-insert (Node {rootLabel = (l, u), subForest = children}) p =
-	Node {
-		rootLabel = (l, u),
-		subForest = instantiateSubdivisions (l, u) newKids
-	}
-	where
-	newKids = insert' children p
-
--- Takes the list of children from some node and inserts a Point
--- either as a child, or into a child
-insert' :: [PKTree] -> Point -> [PKTree]
-insert' children p
-	| children `contains` p =
-		let (Just c) = children `maybecontain` p in
-			let c' = insert c p in
-				-- If c' has less than k children, it is not k-instantiable
-				(if length (subForest c') < k then
-					subForest c'
-				else
-					[c']
-				) ++ filter (/= c) children
-	| otherwise =
-		pointCell p : children
-	where
-	contains f p = isJust (maybecontain f p)
-	maybecontain f p = find (\x -> rectContains (rootLabel x) (p,p)) f
-
-cell :: Rectangle -> PKTree
-cell r = Node {
-	rootLabel = r,
-	subForest = []
-}
-
-pointCell :: Point -> PKTree
-pointCell p = cell (p, p)
-
-rectContains :: Rectangle -> Rectangle -> Bool
-rectContains (l, u) (l', u') =
-	and (compareEach (<) l l') && and (compareEach (>) u u')
-	where
-	compareEach _ [] _ = []
-	compareEach _ _ [] = []
-	compareEach f (a:as) (b:bs) = f a b : compareEach f as bs
